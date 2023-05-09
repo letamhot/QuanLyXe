671bc5a48edf493645c1cf35cf9cd21161fcc3d7
letamhot <letamhot@gmail.com>
2023-05-09 07:49:45 +0700

    cm_update_baocaotonghop

---
+++ QLDV/Controllers/BaoCaoTongHopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLDV.Controllers
+{
+    public class BaoCaoTongHopController : Controller
+    {
+        // GET: BaoCaoTongHop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
